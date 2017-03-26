a98c1181bf67fc39045146b45e2fd6c7f50fb48d
Nico Feulner <nico.feulner@gmail.com>
2017-03-26 14:38:46 +0200

    initial commit

---
+++ Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH := $(call my-dir)
+
+# Install Tuga Browser
+include $(CLEAR_VARS)
+LOCAL_MODULE := SWE
+LOCAL_SRC_FILES := SWE/SWE.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_PRIVILEGED_MODULE := false
+LOCAL_OVERRIDES_PACKAGES := Browser Gello Email Exchange
+include $(BUILD_PREBUILT)
