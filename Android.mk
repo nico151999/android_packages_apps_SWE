5c56492c3e5916f4e3eb98ddfe17c61039153177
Nico Feulner <nico.feulner@gmail.com>
2017-04-08 20:02:04 +0200

    Also override Exchange2
    Change-Id: I6529f4755b06d819d9bd0f7f29f4d42adcf48ced

--- Android.mk
+++ Android.mk
@@ -9,5 +9,5 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_PRIVILEGED_MODULE := false
 # List Email and Exchange to disable preinstalled Mail clients at all
-LOCAL_OVERRIDES_PACKAGES := Browser Gello Email Exchange
+LOCAL_OVERRIDES_PACKAGES := Browser Gello Email Exchange Exchange2
 include $(BUILD_PREBUILT)
